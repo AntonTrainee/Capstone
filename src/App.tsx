b76ad8d9381aab62901e2d9b6892bb2ceef04285
AntonTrainee <antonulrichtorres@gmail.com>
2025-10-21 02:55:09 +0000

    go

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import { ReviewsProvider } from "./pages/ReviewsContext"; 
 
-// Pages
-import Home from "./pages/Home";
-import AdminReviews from "./pages/AdminReviews";
 import OtpPage from "./pages/OtpPage";
+import ProtectedRoute from "./ProtectedRoute";
+import AdminProtectedRoute from "./AdminProtectedRoute";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import CustomerDashb from "./pages/customerdashb";
 import Booksys from "./pages/booksys";
 import Admindashb from "./pages/admindashb";
+
 import About from "./pages/aboutus";
 import Services from "./pages/services";
 import Manageb from "./pages/manageb";
@@ -22,27 +24,20 @@ import BeforeAfterAdd from "./pages/beforeandafteradd";
 import BeforeAndAfterShowcase from "./pages/beforeandaftershowcase";
 import PrivacyNotice from "./pages/PrivacyNotice";
 import Cookies from "./pages/cookies";
-
-// Components & Context
 import CookieBanner from "./components/CookieBanner";
-import ProtectedRoute from "./ProtectedRoute";
-import AdminProtectedRoute from "./AdminProtectedRoute";
-import { ReviewsProvider } from "./pages/ReviewsContext";
+
+
+import AdminReviews from "./pages/AdminReviews";
 
 function App() {
   return (
-    <>
+    <ReviewsProvider> 
       <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={
-          <ReviewsProvider>
-            <Home />
-          </ReviewsProvider>
-        } />
         <Route path="/services" element={<Services />} />
         <Route path="/register" element={<Register />} />
         <Route path="/otp" element={<OtpPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/aboutus" element={<About />} />
         <Route path="/beforeandaftershowcase" element={<BeforeAndAfterShowcase />} />
         <Route path="/privacy-notice" element={<PrivacyNotice />} />
@@ -50,31 +45,28 @@ function App() {
         <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/resetpassword" element={<ResetPassword />} />
 
-        {/* Protected Customer Routes */}
         <Route element={<ProtectedRoute />}>
           <Route path="/customerdashb" element={<CustomerDashb />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/booksys" element={<Booksys />} />
         </Route>
 
-        {/* Protected Admin Routes */}
         <Route element={<AdminProtectedRoute />}>
           <Route path="/admindashb" element={<Admindashb />} />
           <Route path="/manageb" element={<Manageb />} />
           <Route path="/salesandreq" element={<Salesandreq />} />
           <Route path="/analytics" element={<Analytics />} />
           <Route path="/beforeafter" element={<BeforeAfter />} />
+          <Route path="/" element={<AdminReviews />} />
           <Route path="/beforeafter/add" element={<BeforeAfterAdd />} />
-          <Route path="/admin-reviews" element={
-            <ReviewsProvider>
-              <AdminReviews />
-            </ReviewsProvider>
-          } />
+
+          
+          <Route path="/admin-reviews" element={<AdminReviews />} />
         </Route>
       </Routes>
 
       <CookieBanner />
-    </>
+    </ReviewsProvider>
   );
 }
 
